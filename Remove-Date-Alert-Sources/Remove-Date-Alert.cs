3c820d5707e4b4d12b6e6591d7cddfab91c8fad6
Mr.ture.China <mr.true.china@gmail.com>
2021-09-14 11:20:22 +0800

    1.0.0

---
+++ Remove-Date-Alert-Sources/Remove-Date-Alert.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using UnityModManagerNet;
+
+namespace MtC.Mod.ChineseParent.RemoveDateAlert
+{
+    public static class Main
+    {
+        /// <summary>
+        /// Mod 对象
+        /// </summary>
+        public static UnityModManager.ModEntry ModEntry { get; set; }
+
+        /// <summary>
+        /// 这个 Mod 是否启动
+        /// </summary>
+        public static bool enabled;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            // 保存 Mod 对象
+            ModEntry = modEntry;
+
+            // 加载 Harmony
+            var harmony = new Harmony(modEntry.Info.Id);
+            harmony.PatchAll();
+
+            modEntry.Logger.Log("移除约会警告 Mod 加载完成");
+            
+            // 返回加载成功
+            return true;
+        }
+    }
+
+    /// <summary>
+    /// 约会发出警告事件的补丁
+    /// </summary>
+    [HarmonyPatch(typeof(panel_girls), "add_alert")]
+    public static class AddAlertPatch
+    {
+        private static bool Prefix()
+        {
+            Main.ModEntry.Logger.Log("约会警告即将发出");
+            
+            // 如果这个 Mod 已启动则返回 false，表示不调用约会警告发出方法
+            return !Main.enabled;
+        }
+    }
+}
