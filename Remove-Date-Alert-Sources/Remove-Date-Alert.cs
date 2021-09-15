5785c3d13522bb1953babc756181f565d4cb8836
Mr.ture.China <mr.true.china@gmail.com>
2021-09-15 12:05:24 +0800

    添加对开关 Mod 的相应

--- Remove-Date-Alert-Sources/Remove-Date-Alert.cs
+++ Remove-Date-Alert-Sources/Remove-Date-Alert.cs
@@ -19,6 +19,7 @@ namespace MtC.Mod.ChineseParent.RemoveDateAlert
         {
             // 保存 Mod 对象
             ModEntry = modEntry;
+            ModEntry.OnToggle = OnToggle;
 
             // 加载 Harmony
             var harmony = new Harmony(modEntry.Info.Id);
@@ -28,6 +29,21 @@ namespace MtC.Mod.ChineseParent.RemoveDateAlert
             
             // 返回加载成功
             return true;
+        }
+
+        /// <summary>
+        /// Mod Manager 对 Mod 进行控制的时候会调用这个方法
+        /// </summary>
+        /// <param name="modEntry"></param>
+        /// <param name="value">这个 Mod 是否激活</param>
+        /// <returns></returns>
+        static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            // 将 Mod Manager 切换的状态保存下来
+            enabled = value;
+
+            // 返回 true 表示这个 Mod 切换到 Mod Manager 切换的状态，返回 false 表示 Mod 依然保持原来的状态
+            return true; // If true, the mod will switch the state. If not, the state will not change.
         }
     }
 
@@ -39,10 +55,16 @@ namespace MtC.Mod.ChineseParent.RemoveDateAlert
     {
         private static bool Prefix()
         {
+            // 如果 Mod 没有启动，直接按照原流程继续调用
+            if (!Main.enabled)
+            {
+                return true;
+            }
+
             Main.ModEntry.Logger.Log("约会警告即将发出");
             
-            // 如果这个 Mod 已启动则返回 false，表示不调用约会警告发出方法
-            return !Main.enabled;
+            // 阻断对发出警告方法的调用
+            return false;
         }
     }
 }
